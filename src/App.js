97afe2349d5b8ba4803d642f54be4d9a21c15e1d
berketekerci <ahmetb.tekerci@gmail.com>
2023-12-05 23:06:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Counter from "./components/Counter.js"
+import IncreaseCounter from "./components/IncreaseCounter";
+import DecreaseCounter from "./components/DecreaseCounter";
+import IncreaseByTwoCounter from "./components/IncreaseByTwoCounter";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <IncreaseCounter />
+      <DecreaseCounter />
+      <IncreaseByTwoCounter />
+    </>
+  );
+}
+
+export default App;
